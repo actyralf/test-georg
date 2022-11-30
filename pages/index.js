b089470df3e5c0406383686c1c5363f0d2fabe78
Georg Bonin <114496506+GeBon22@users.noreply.github.com>
2022-11-30 15:32:54 +0100

    left dots

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function HomePage() {
       render: "svg",
       loop: true,
       autoplay: true,
-      animationData: require("../public/BestellungAnimation"),
+      animationData: require("/public/BestellungAnimation"),
     });
   }, []);
 
@@ -23,11 +23,11 @@ export default function HomePage() {
       render: "svg",
       loop: true,
       autoplay: true,
-      animationData: require("../public/WiderrufAnimation"),
+      animationData: require("/public/WiderrufAnimation"),
     });
   }, []);
 
-  console.log(require("../public/WiderrufAnimation"));
+  console.log(require("/public/WiderrufAnimation"));
 
   return (
     <div>
